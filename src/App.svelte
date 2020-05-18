38f36254b57ec0d8887567d34b10ef3f7d2271c1
Tanushka PC <tanu31195@users.noreply.github.com>
2020-05-19 01:24:27 +0530

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,90 @@
+<script>
+
+</script>
+
+<main>
+    <form>
+        <div class="field">
+            <input type="email" name="email" class="input" placeholder=""/>
+            <label for="email" class="label">Email</label>
+        </div>
+
+        <div class="field">
+            <input type="password" name="password" class="input" placeholder=""/>
+            <label for="password" class="label">Password</label>
+        </div>
+
+    </form>
+</main>
+
+<style>
+    form {
+        --text-color: #afafaf;
+        max-width: 500px;
+    }
+
+    .field {
+        width: 100%;
+        position: relative;
+        border-bottom: 2px dashed var(--text-color);
+        margin: 4rem auto 1rem;
+    }
+
+    .label {
+        outline: none;
+        border: none;
+        overflow: hidden;
+        margin: 0;
+        width: 100%;
+        padding: 0.25rem 0;
+        background: none;
+        color: white;
+        font-size: 1.2rem;
+        font-weight: bold;
+    }
+
+    .input:valid {
+        color: yellowgreen;
+    }
+
+    .input:invalid {
+        color: orangered;
+    }
+
+/*	Border Animation */
+
+    .field::after {
+        content: "";
+        position: relative;
+        height: 4px;
+        width: 100%;
+        background: #d16dff;
+        transform: scaleX(0);
+        transform-origin: 0; /* left to right. Set to 100 for right to left */
+        transition: transform 500ms ease;
+        top: 2px;
+    }
+
+    .field:focus-within {
+        border-color: transparent;
+    }
+
+    .field:focus-within::after {
+        transform: scaleX(1);
+    }
+
+/*    Label Animation */
+    .label {
+        z-index: -1;
+        position: absolute;
+        transform: translateY(-2rem);
+        transform-origin: 0;
+        transition: transform 400ms;
+    }
+
+    .field:focus-within .label,
+    .input:not(:placeholder-shown) + .label {
+        transform: scale(0.8) translateY(-5rem);
+    }
+
+</style>
